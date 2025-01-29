9617a159e4f0830885a3371c1acd9884f8c08605
sanjithrk06 <sanjithkarthikeyanr@gmail.com>
2025-01-29 09:05:26 +0530

    Cors updation

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ import {
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors());
 
 // Middlewares
 app.use(express.json());
